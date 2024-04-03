3c5ec70b56279766c2f66e54947f43d11c48cf5c
Angel-HF <49835792+Pelochus@users.noreply.github.com>
2024-04-03 18:38:24 +0200

    Added -c option to ntop.sh

--- ntop.sh
+++ ntop.sh
@@ -2,8 +2,18 @@
 # Author: Pelochus
 # Brief: A very basic 'top' style program that shows the status of the NPU in Rockchip's SoCs 
 
+# Variables
+$CLEAR
+
+# Parameters check
+if [ $1 -eq "-h" ]; then
+    # TODO: Help menu
+elif [ $1 -eq "-c" ]; then
+    $CLEAR=clear
+fi
+
 while true; do
-    clear
+    $CLEAR
     cat /sys/kernel/debug/rknpu/load
     sleep 0.5
 done
